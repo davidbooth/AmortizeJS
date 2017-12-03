190519b96ef2bf6c2729dc32c3a45ba26fe4198f
dabo <david.developments@gmail.com>
2017-12-03 17:06:56 -0500

    feat: updated interface to support dates

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -16,6 +16,10 @@ export interface AmortizationMethod{
     periodicInterest:    number;     //The interest payed per period, if the period is month then divide APR/12 (ex: APR = 3.5%, i = 0.035/12)
     periodicPayment:     number;     //The total payment that needs to be made per period.
     schedule:            Payment[];  //Array of payments required to pay off the balance. (Amortization Schedule)
+    totalPayment:        number;     //The total of all payments over the term.
+    totalInterest:       number;     //The total amount of interest spend over the term.
+    startDate?:          Date;       //The start date of the loan.
+    endDate?:            Date;       //The pay off date (Will only be calculated if startDate was given).
 
 }
 
